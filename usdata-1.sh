abb7941fc3117f109a5a6cf68ca856770870a663
EC2 Default User <ec2-user@ip-172-31-41-251.ec2.internal>
2023-10-12 06:25:54 +0000

    sdf

--- usdata-1.sh
+++ usdata-1.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 sudo yum update -y
-sudo yum -y install httpd
-sudo systemctl start httpd
-sudo systemctl enable httpd
+sudo yum -y install nginx1.12
+sudo systemctl start nginx.service
+sudo systemctl enable nginx.service
+sudo systemctl restart nginx.service
 sudo yum install -y git
 sudo git clone https://github.com/anilkumar3577/food.git /var/www/html
 
